bbb8782e8469b9b9d4b636d2329ea31b6c0c4ad6
Daudft <alikhaft45@gmail.com>
2026-04-09 11:33:33 +0500

    day 2-4 done

--- day1/server.js
+++ day1/server.js
@@ -2,20 +2,22 @@ const express = require("express");
 
 const app = express(); //server instance ban gaya 
 
-app.get('/',(req,res) =>{
-    res.send("Hello World");
+app.use(express.json());
+
+const notes = [];
+
+app.post('/notes',(req,res)=>{
+    console.log(req.body);
+    notes.push(req.body);
+
+    res.send("Note added");
 })
 
-app.get('/home',(req,res)=>{
-    res.send("This is Home Page");
+app.get('/notes',(req,res)=>{
+    res.send(notes);
 })
 
-app.get('/about',(req,res)=>{
-    res.send("This is about page")
-})
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
 
-app.get('/contact',(req,res)=>{
-    res.send("This is contact page");
-})
-
-app.listen(3000);//server ko 3000 port pe listen karne ke liye kaha
